78e7e62cdedd7f0aded67df7704c917b7f7c3133
PrestonRooker <prestonrooker@gmail.com>
2021-08-26 06:51:47 -0700

    Update gamebouino.ino
    adding graphics initialization

--- gamebouino.ino
+++ gamebouino.ino
@@ -6,12 +6,35 @@ GameBuino by Preston Rooker
 #include <Adafruit_GFX.h>
 #include <Adafruit_SSD1306.h>
 
-void setup() {
-  // put your setup code here, to run once:
+#define SCREEN_WIDTH 128 // OLED display width, in pixels
+#define SCREEN_HEIGHT 64 // OLED display height, in pixels
+#define OLED_RESET     -1 // Reset pin # (or -1 if sharing Arduino reset pin)
+#define SCREEN_ADDRESS 0x3C ///< See datasheet for Address (generally 0x3D or 0x3C)
+Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 
+void setup() 
+{
+  // put your setup code here, to run once:
+  Serial.begin(9600);
+
+  // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
+  if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
+    Serial.println(F("SSD1306 allocation failed"));
+    for(;;); // Don't proceed, loop forever
+  }
+
+  display.display();
+  delay(2000); // Pause for 2 seconds
+
+  // Clear the buffer
+  display.clearDisplay();
+  display.display();
+  Serial.println(display.width());
+  Serial.println(display.height());
 }
 
-void loop() {
+void loop() 
+{
   // put your main code here, to run repeatedly:
 
 }
