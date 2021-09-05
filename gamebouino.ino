ec386b55e606e7c21fcfbf8bf94197ea38cf5cdc
PrestonRooker <prestonrooker@gmail.com>
2021-09-04 21:57:37 -0700

    added game over text which shows your score

--- gamebouino.ino
+++ gamebouino.ino
@@ -24,16 +24,18 @@ Adafruit_SSD1306 display(SCREEN_WIDTH, SCREEN_HEIGHT, &Wire, OLED_RESET);
 #define SNAKE 5
 
 #define FRAMERATECONST 130
+//#define FRAMERATECONST 67
 
 //344  bytes are used just by the libraries and display construction >:(
 //1100 bytes are needed for the OLED buffer
+//apparently sometimes you need 1200 bytes for OLED buffer, its hard to tell
+//okay so I did the math and it takes (or should take) 1136 bytes
 //so good luck working with like only 800 bytes
 
 
 //snake
 byte dir = RIGHT;
-//uint16_t length = 3;
-//uint16_t numSnakeBlocks = 1; //can only use one uint16_t for this by simply putting detection for removing tail pixel in placement pixel and then just not removing a tail pixel when food is eaten
+uint16_t score = 0; //score increases each time a player eats a food
 byte headX;
 byte headY;
 byte tailX;
@@ -53,17 +55,19 @@ void drawBlock(byte x, byte y, uint16_t color = SSD1306_WHITE); //apparently for
 void setup() 
 {
   // put your setup code here, to run once:
-  //Serial.begin(9600); //REMOVE ON RELEASE
-  pinMode(VRX_PIN,INPUT);
-  pinMode(VRY_PIN,INPUT);
-  pinMode(SW_PIN,INPUT_PULLUP);
+
 
   // SSD1306_SWITCHCAPVCC = generate display voltage from 3.3V internally
   if(!display.begin(SSD1306_SWITCHCAPVCC, SCREEN_ADDRESS)) {
-    //Serial.println(F("SSD1306 allocation failed")); //REMOVE ON RELEASE
     pinMode(LED_BUILTIN, OUTPUT);
-    digitalWrite(LED_BUILTIN, HIGH);
-    for(;;); // Don't proceed, loop forever
+    
+    // Don't proceed, loop forever
+    for(;;){
+      digitalWrite(LED_BUILTIN, HIGH);
+      delay(300);
+      digitalWrite(LED_BUILTIN, LOW);
+      delay(2000);  
+    }
   }
 
   for(byte x = 0; x < BOARD_WIDTH; x++){
@@ -71,9 +75,14 @@ void setup()
       board[x][y] = NONE; //set each cell to NONE
     }
   }
+  
+  pinMode(VRX_PIN,INPUT);
+  pinMode(VRY_PIN,INPUT);
+  pinMode(SW_PIN,INPUT_PULLUP);
+  
+  randomSeed(A2); //A2 has nothing connected to it, which causes noise which seeds the random
+  //randomSeed(12);
 
-  //randomSeed(A2); //A2 has nothing connected to it, which causes noise which seeds the random
-  randomSeed(12);
   // Clear the buffer
   display.clearDisplay();
 
@@ -92,8 +101,6 @@ void setup()
   board[tailX][tailY] = UP;
   display.display();
   delay(1000);
-
-  display.display();
 }
 
 void loop() 
@@ -105,6 +112,7 @@ void loop()
   moveSnake();
   display.display();
   delay(FRAMERATECONST -(millis() - initTime));
+  //runGameOver();
 }
 
 //code food drawing
@@ -239,6 +247,7 @@ void moveSnake(){
     }
   }
   else{
+    score++;
     generateFood();
   }
   detectCollision();
@@ -253,23 +262,104 @@ void detectCollision(){
 }
 
 void runGameOver(){
-  display.invertDisplay(true);
-  delay(200);
-  display.invertDisplay(false);
-  delay(200);
-  display.invertDisplay(true);
-  delay(200);
-  display.invertDisplay(false);
-  delay(200);
-  display.invertDisplay(true);
-  delay(200);
-  display.invertDisplay(false);
-  delay(200);
-  display.invertDisplay(true);
-  delay(200);
-  display.invertDisplay(false);
-  delay(200);
+  bool toggle = false;
+  for(byte i = 0; i < 8; i++){
+    toggle = !toggle;
+    display.invertDisplay(toggle);
+    delay(200);
+  }
   display.clearDisplay();
+  display.setTextColor(SSD1306_WHITE);
+
+  //text size 1 is 6x8 pixels, other sizes just multiply, so size 2 is (6*2)x(8*2) so 12x16
+  
+  display.setTextSize(2);
+   display.setCursor(10,display.height()/4);
+  display.println(F("GAME OVER"));
+  
+  display.setTextSize(1);    
+  display.setCursor(31,45);
+  display.print(F("Score: "));
+
+  if(score > 199){
+    display.print(F("2"));
+    score = score - 200;
+  }
+  else if(score > 99){
+    display.print(F("1"));
+    score = score - 100;
+  }
+
+  if(score < 10){
+    display.print(F("0"));
+  }
+  else if(score < 20){
+    display.print(F("1"));
+    score = score - 10;
+  }
+  else if(score < 30){
+    display.print(F("2"));
+    score = score - 20;
+  }
+  else if(score < 40){
+    display.print(F("3"));
+    score = score - 30;
+  }
+  else if(score < 50){
+    display.print(F("4"));
+    score = score - 40;
+  }
+  else if(score < 60){
+    display.print(F("5"));
+    score = score - 10;
+  }
+  else if(score < 70){
+    display.print(F("6"));
+    score = score - 60;
+  }
+  else if(score < 80){
+    display.print(F("7"));
+    score = score - 70;
+  }
+  else if(score < 90){
+    display.print(F("8"));
+    score = score - 80;
+  }
+  else if(score < 100){
+    display.print(F("9"));
+    score = score - 90;
+  }
+
+  if(score < 1){
+    display.print(F("0"));
+  }
+  else if(score < 2){
+    display.print(F("1"));
+  }
+  else if(score < 3){
+    display.print(F("2"));
+  }
+  else if(score < 4){
+    display.print(F("3"));
+  }
+  else if(score < 5){
+    display.print(F("4"));
+  }
+  else if(score < 6){
+    display.print(F("5"));
+  }
+  else if(score < 7){
+    display.print(F("6"));
+  }
+  else if(score < 8){
+    display.print(F("7"));
+  }
+  else if(score < 9){
+    display.print(F("8"));
+  }
+  else if(score < 10){
+    display.print(F("9"));
+  }
   display.display();
   for(;;); //Don't proceed, loop forever
 }
